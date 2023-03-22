00514cd8f19765eb7bcf3a63a29483740b939615
Talkhadzhus <115663968+Talkhadzhus@users.noreply.github.com>
2023-03-22 17:20:30 +0200

    Add files via upload

---
+++ JSLesson1/HomeWorkRask2.js
@@ -0,0 +1,4 @@
+let FirstSizeValue = prompt('Length of the first side of the rectangle...');
+let SecondSizeValue = prompt('The length of the second side of the rectangle...');
+
+console.log('The total area of a rectangle is ... ', FirstSizeValue * SecondSizeValue);
