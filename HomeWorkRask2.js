b34323863a2fa86167887fba7dc1b950b24d59a2
Talkhadzhus <115663968+Talkhadzhus@users.noreply.github.com>
2023-03-22 17:21:28 +0200

    Delete HomeWorkRask2.js

--- HomeWorkRask2.js
+++
@@ -1,4 +0,0 @@
-let FirstSizeValue = prompt('Length of the first side of the rectangle...');
-let SecondSizeValue = prompt('The length of the second side of the rectangle...');
-
-console.log('The total area of a rectangle is ... ', FirstSizeValue * SecondSizeValue);
