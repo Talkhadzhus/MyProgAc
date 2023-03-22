9c2ae5336c68e53286d579571bf8e1733f7b4d1c
Talkhadzhus <115663968+Talkhadzhus@users.noreply.github.com>
2023-03-22 17:21:36 +0200

    Delete HomeWorkTask1.js

--- HomeWorkTask1.js
+++
@@ -1,5 +0,0 @@
-
-let PriceValue = prompt('Please, enter price of the Tools');
-
-console.log('Your sale will be 3% if you buy 10 Tools ', PriceValue * 0.97);
-console.log('Your sale will be 5% if you buy 20 Tools ', PriceValue * 0.95);
